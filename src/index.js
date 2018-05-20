8695b60f70bd7647b0bf6271b02764687c302f88
juicylevel <rykov87yuri@gmail.com>
2018-05-20 11:48:52 +0300

    add normalize css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './sass/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import 'normalize.css';
+import './sass/index.css';
 
 ReactDOM.render(
     <App />, 
