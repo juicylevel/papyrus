4d94a36736f4849556b687c7e82b4475d4014ae6
juicylevel <rykov87yuri@gmail.com>
2018-05-20 10:56:37 +0300

    add project files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render () {
+        return (
+            <div>
+                papyrus
+            </div>
+        );
+    }
+}
+
+export default App;
